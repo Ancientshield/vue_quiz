e0ac2385c1249240fff13bf6428cd812393fa4f1
Ancientshield <jacklin2100@gmail.com>
2023-04-03 23:10:37 +0800

    add TransitionGroup, Transition, and CSS in this quiz

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -10,25 +10,27 @@
 			</div>
 		</div>
 		<!-- qi 是 question index 縮寫，由 v-for 回圈自動產生 跟 questions 無關 -->
-		<div
-			class="single-question"
-			v-for="(question, qi) in questions"
-			:key="question.q"
-			v-show="questionsAnswered === qi"
-		>
-			<div class="question">{{ question.q }}</div>
-			<div class="answers">
-				<!-- answer.is_correct 是 App.vue Array 內的一個值 -->
-				<div
-					class="answer"
-					v-for="answer in question.answers"
-					key="answer.text"
-					@click.pervent="selectAnswer(answer.is_correct)"
-				>
-					{{ answer.text }}
+		<TransitionGroup name="fade">
+			<div
+				class="single-question"
+				v-for="(question, qi) in questions"
+				:key="question.q"
+				v-show="questionsAnswered === qi"
+			>
+				<div class="question">{{ question.q }}</div>
+				<div class="answers">
+					<!-- answer.is_correct 是 App.vue Array 內的一個值 -->
+					<div
+						class="answer"
+						v-for="answer in question.answers"
+						key="answer.text"
+						@click.pervent="selectAnswer(answer.is_correct)"
+					>
+						{{ answer.text }}
+					</div>
 				</div>
 			</div>
-		</div>
+		</TransitionGroup>
 	</div>
 </template>
 
